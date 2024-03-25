c719ed7db7ac46802e592a0879a6ac0799652300
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-03-25 11:32:55 +0600

    added dark mode

--- src/components/AllSection/Header/Header.jsx
+++ src/components/AllSection/Header/Header.jsx
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Header = () => {
-  return <div>This is header</div>;
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    if (theme === "light") {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+  return (
+    <div className="flex justify-between items-center  bg-white dark:bg-black dark:text-white">
+      This is header
+      <button
+        className="bg-green-200 text-black px-4 py-2 rounded-3xl shadow-sm hover:shadow-lg"
+        onClick={toggleTheme}
+      >
+        Dark Mode
+      </button>
+    </div>
+  );
 };
 
 export default Header;
